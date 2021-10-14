01052a8b52db33266b6d7453abb9c35d7564bafe
Nik6996 <nik.taraskov@ukr.net>
2021-10-14 04:19:50 +0400

    /

--- src/store/modules/bannerModule.js
+++ src/store/modules/bannerModule.js
@@ -1,6 +1,6 @@
 import { database } from "../../firebaseConfig";
 
-import { getDatabase, ref, set, onValue } from "firebase/database";
+import { getDatabase, ref, set, child, get } from "firebase/database";
 
 
 export default {
@@ -12,32 +12,45 @@ export default {
 			return state.banners
 		}
 	},
-	actions: {
-		// async pushName({ commit }, name) {
-		// 	const database = getDatabase();
-		// 	set(ref(database, 'cinema'), { name })
-
+	mutations: {
+		loadBanner(state, payload) {
+			state.banners = payload
+		}
+		// loadBanner(state, payload) {
+		// 	state.banners = payload
 		// }
+
+	},
+	actions: {
+
 		async saveBanners({ commit }, banners) {
 			const database = getDatabase();
 			set(ref(database, 'banners'), { banners })
 
 		},
-		// async loadBanners() {
-		// 	const database = getDatabase();
-		// 	const bannersData = ref(db, 'banners');
-		// 	onValue(bannersData, (snapshot) => {
-		// 		const data = snapshot.val();
-		// 		updateStarCount(data);
-		// 	});
-		// }
-
-
-
+		async loadBanners() {
+			const database = ref(getDatabase());
+			get(child(database, 'banners')).then((banner) => {
+				if (banner.exists()) {
+					let bannerVal = banner.val().banners
+					const bannersArray = [];
+					Object.keys(bannerVal).forEach(key => {
+						const bannerPush = bannerVal[key];
+						bannersArray.push(bannerPush.url, bannerPush.text)
+					});
+					commit('loadBanner', bannersArray);
+					// commit('loadBanner', bannerVal);
+					console.log(bannersArray);
+					console.log(banner.val());
+				} else {
+					console.log("No data available");
+				}
+			}).catch((error) => {
+				console.error(error);
+			});
+		}
 
 	},
-	mutations: {
 
-	},
 
 }
