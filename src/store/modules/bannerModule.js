b2b19c71b27474b5b6864b0f845ee49bc34c9d0c
Nik6996 <nik.taraskov@ukr.net>
2021-11-05 13:38:07 +0400

    /

--- src/store/modules/bannerModule.js
+++ src/store/modules/bannerModule.js
@@ -9,172 +9,172 @@ const default_interval = 5;
 const BANNERS_DATABASE_PATH = 'banners';
 
 export const bannerModule = {
-	namespaced: true,
+  namespaced: true,
 
-	state: () => ({
-		items: [],
-		interval: default_interval,
-		error: null,
-		isLoading: true,
+  state: () => ({
+    items: [],
+    interval: default_interval,
+    error: null,
+    isLoading: true,
 
-	}),
-	getters: {
-		items(state) {
-			return state.items;
-		},
-		interval(state) {
-			return state.interval;
-		},
-		isLoading(state) {
-			return state.isLoading;
-		},
-		error(state) {
-			return state.error;
-		},
+  }),
+  getters: {
+    items(state) {
+      return state.items;
+    },
+    interval(state) {
+      return state.interval;
+    },
+    isLoading(state) {
+      return state.isLoading;
+    },
+    error(state) {
+      return state.error;
+    },
 
-	},
+  },
 
-	actions: {
-		async save({ commit, state }, { items, interval }) {
-			commit('setError', null);
-			commit('setIsLoading', true);
+  actions: {
+    async save({ commit, state }, { items, interval }) {
+      commit('setError', null);
+      commit('setIsLoading', true);
 
-			try {
-				const itemsRef = ref(database, `${BANNERS_DATABASE_PATH}/items`)
-				const itemsRecord = await get(itemsRef)
-				if (itemsRecord.exists()) {
-					itemsRecord.forEach(itemRecord => {
+      try {
+        const itemsRef = ref(database, `${BANNERS_DATABASE_PATH}/items`)
+        const itemsRecord = await get(itemsRef)
+        if (itemsRecord.exists()) {
+          itemsRecord.forEach(itemRecord => {
 
-						const databaseItem = itemRecord.val();
-						const matchingItem = items.find(item => databaseItem.id === item.id);
-						if (!matchingItem) {
-							set(ref(database, `${BANNERS_DATABASE_PATH}/items/${databaseItem.id}`), null);
-							const storageRef = refStorage(storage, `banners/${databaseItem.id}`);
-							deleteObject(storageRef);
-						}
-					})
-					items.forEach((item, index) => {
+            const databaseItem = itemRecord.val();
+            const matchingItem = items.find(item => databaseItem.id === item.id);
+            if (!matchingItem) {
+              set(ref(database, `${BANNERS_DATABASE_PATH}/items/${databaseItem.id}`), null);
+              const storageRef = refStorage(storage, `banners/${databaseItem.id}`);
+              deleteObject(storageRef);
+            }
+          })
+          items.forEach((item, index) => {
 
-						if (item.image === '') {
-							async function updateImg() {
-								const img = item.file
+            if (item.image === '') {
+              async function updateImg() {
+                const img = item.file
 
-								const storageRef = refStorage(storage, `banners/${item.file.imgId}`);
-								await uploadBytes(storageRef, img);
-								const url = await getDownloadURL(refStorage(storage, `banners/${item.file.imgId}`))
+                const storageRef = refStorage(storage, `banners/${item.file.imgId}`);
+                await uploadBytes(storageRef, img);
+                const url = await getDownloadURL(refStorage(storage, `banners/${item.file.imgId}`))
 
-								set(ref(database, `${BANNERS_DATABASE_PATH}/items/${item.id}`), {
-									...item,
-									order: index,
-									image: url
-								})
+                set(ref(database, `${BANNERS_DATABASE_PATH}/items/${item.id}`), {
+                  ...item,
+                  order: index,
+                  image: url
+                })
 
-							}
-							updateImg()
+              }
+              updateImg()
 
-						} else {
-							set(ref(database, `${BANNERS_DATABASE_PATH}/items/${item.id}`), {
-								...item,
-								order: index,
+            } else {
+              set(ref(database, `${BANNERS_DATABASE_PATH}/items/${item.id}`), {
+                ...item,
+                order: index,
 
-							})
-						}
-					});
-				} else {
-					items.forEach((item, index) => {
-
-
-						if (item.file) {
-							// TODO: update file
-
-							async function updateImg() {
-								const img = item.file
-
-								const storageRef = refStorage(storage, `banners/${item.file.imgId}`);
-								await uploadBytes(storageRef, img);
-								const url = await getDownloadURL(refStorage(storage, `banners/${item.file.imgId}`))
-
-								set(ref(database, `${BANNERS_DATABASE_PATH}/items/${item.id}`), {
-									...item,
-									order: index,
-									image: url
-								})
-
-							}
-							updateImg()
+              })
+            }
+          });
+        } else {
+          items.forEach((item, index) => {
 
 
-						} else {
-							set(ref(database, `${BANNERS_DATABASE_PATH}/items/${item.id}`), {
-								...item,
-								order: index,
-							})
-						}
+            if (item.file) {
+              // TODO: update file
 
-					});
-				}
+              async function updateImg() {
+                const img = item.file
 
-				const intervalRef = ref(database, `${BANNERS_DATABASE_PATH}/interval`);
-				set(intervalRef, interval);
+                const storageRef = refStorage(storage, `banners/${item.file.imgId}`);
+                await uploadBytes(storageRef, img);
+                const url = await getDownloadURL(refStorage(storage, `banners/${item.file.imgId}`))
 
-				commit('setItems', items);
-				commit('setInterval', interval);
+                set(ref(database, `${BANNERS_DATABASE_PATH}/items/${item.id}`), {
+                  ...item,
+                  order: index,
+                  image: url
+                })
 
-			} catch (error) {
-				commit('setError', null);
+              }
+              updateImg()
 
-			} finally {
-				commit('setIsLoading', false)
-			}
-		},
 
-		async load({ commit }) {
-			commit('setError', null);
-			commit('setIsLoading', true);
+            } else {
+              set(ref(database, `${BANNERS_DATABASE_PATH}/items/${item.id}`), {
+                ...item,
+                order: index,
+              })
+            }
 
-			try {
-				const itemsRef = ref(database, `${BANNERS_DATABASE_PATH}/items`);
-				const itemsQuery = query(itemsRef, orderByChild("order"));
-				const itemsRecord = await get(itemsQuery);
+          });
+        }
 
-				if (itemsRecord.exists()) {
-					const items = [];
-					itemsRecord.forEach(itemRecord => {
-						items.push(itemRecord.val())
-					});
-					commit('setItems', items)
-				}
-				const intervalRef = ref(database, `${BANNERS_DATABASE_PATH}/interval`);
-				const intervalRecord = await get(intervalRef);
+        const intervalRef = ref(database, `${BANNERS_DATABASE_PATH}/interval`);
+        set(intervalRef, interval);
 
-				if (intervalRecord.exists()) {
-					commit('setInterval', intervalRecord.val())
-				}
+        commit('setItems', items);
+        commit('setInterval', interval);
 
-			} catch (error) {
-				commit('setError', error);
-			}
-			finally {
-				commit('setIsLoading', false);
-			}
-		}
-	},
+      } catch (error) {
+        commit('setError', null);
 
-	mutations: {
+      } finally {
+        commit('setIsLoading', false)
+      }
+    },
 
-		setInterval(state, interval) {
-			state.interval = interval;
-		},
-		setItems(state, items) {
-			state.items = items
-		},
-		setError(state, error) {
-			state.error = error;
-		},
-		setIsLoading(state, isLoading) {
-			state.isLoading = isLoading;
-		},
-	}
+    async load({ commit }) {
+      commit('setError', null);
+      commit('setIsLoading', true);
+
+      try {
+        const itemsRef = ref(database, `${BANNERS_DATABASE_PATH}/items`);
+        const itemsQuery = query(itemsRef, orderByChild("order"));
+        const itemsRecord = await get(itemsQuery);
+
+        if (itemsRecord.exists()) {
+          const items = [];
+          itemsRecord.forEach(itemRecord => {
+            items.push(itemRecord.val())
+          });
+          commit('setItems', items)
+        }
+        const intervalRef = ref(database, `${BANNERS_DATABASE_PATH}/interval`);
+        const intervalRecord = await get(intervalRef);
+
+        if (intervalRecord.exists()) {
+          commit('setInterval', intervalRecord.val())
+        }
+
+      } catch (error) {
+        commit('setError', error);
+      }
+      finally {
+        commit('setIsLoading', false);
+      }
+    }
+  },
+
+  mutations: {
+
+    setInterval(state, interval) {
+      state.interval = interval;
+    },
+    setItems(state, items) {
+      state.items = items
+    },
+    setError(state, error) {
+      state.error = error;
+    },
+    setIsLoading(state, isLoading) {
+      state.isLoading = isLoading;
+    },
+  }
 
 
 }
